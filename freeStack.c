47610cc39b8d7a270a9ba5161070b085171258ac
Nonku-29 <122856617+Nonku-29@users.noreply.github.com>
2023-05-25 08:46:21 +0200

    Update freeStack.c

---
+++ freeStack.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * free_stack - Frees a doubly linked list.
+ * @head: Pointer to the head of the stack.
+ *
+ * Return: void
+ */
+void free_stack(stack_t *head)
+{
+	stack_t *current = head;
+
+	while (current)
+	{
+		stack_t *temp = current;
+		current = current->next;
+		free(temp);
+	}
+}
+
+/**
+ * opcode_stack - Sets the stack mode (top of the stack is the head).
+ * @stack: Pointer to the head of the stack.
+ * @line_number: Line number being executed.
+ *
+ * Return: void
+ */
+void opcode_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+
+	// monty
+}
