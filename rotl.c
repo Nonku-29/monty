fec9df385fb467ba109a129eebd3d7450e9902eb
Nonku-29 <122856617+Nonku-29@users.noreply.github.com>
2023-05-25 10:09:51 +0200

    Update rotl.c

---
+++ rotl.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+/**
+ * f_queue - Function that sets the stack mode to queue (FIFO)
+ * @head: Double pointer to the head of the queue
+ * @counter: Line count
+ *
+ * Return: Nothing
+ */
+void f_queue(stack_t **head, unsigned int counter)
+{
+    (void)head;
+    (void)counter;
+    bus.lifi = 1;
+}
+
+/**
+ * addqueue - Function that adds a node to the tail of the queue
+ * @head: Double pointer to the head of the queue
+ * @n: New value
+ *
+ * Return: Nothing
+ */
+void addqueue(stack_t **head, int n)
+{
+    stack_t *new_node, *temp;
+
+    temp = *head;
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        printf("Error\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->n = n;
+    new_node->next = NULL;
+    if (temp)
+    {
+        while (temp->next)
+            temp = temp->next;
+    }
+    if (!temp)
+    {
+        *head = new_node;
+        new_node->prev = NULL;
+    }
+    else
+    {
+        temp->next = new_node;
+        new_node->prev = temp;
+    }
+}
